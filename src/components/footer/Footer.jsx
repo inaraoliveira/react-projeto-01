02eadef5ca3e19ee0e69f456e59260bb45db1529
Inara Oliveira <inaraolliveira23@hotmail.com>
2021-10-28 03:27:43 -0300

    Cria componente Header e Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../footer/Footer.css'
+
+export default function Footer(){
+    return(
+        <div className='container'>
+            <footer className='footer'>
+                <h3>Projeto em desenvolvimento.</h3>
+                <h3>Aguardem!</h3>
+            </footer>
+        </div>
+    );
+}
